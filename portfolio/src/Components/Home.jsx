b7da74b035cdc7e83adc30eb80c98e8416624cc2
[james-mbuvi] <[james.mbuvi@student.moringaschool.com]>
2024-06-11 20:28:23 +0300

    Deployment

--- portfolio/src/Components/Home.jsx
+++ portfolio/src/Components/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import profileImage from '../assets/Screenshot from 2024-05-15 13-41-15.png'
+
 const Home = () => {
   return (
     <div className="flex flex-col md:flex-row items-center bg-gray-900 text-white min-h-screen p-8 md:p-20">
@@ -6,19 +8,19 @@ const Home = () => {
         <p className="text-lg">Hello, It's Me</p>
         <h1 className="text-4xl md:text-6xl font-bold mt-2">James Mbuvi</h1>
         <h2 className="text-2xl md:text-4xl font-semibold text-teal-400 mt-2">And I'm a Software Developer</h2>
-        <p className="mt-4 text-gray-300">Passionate software engineer skilled in HTML5, CSS, JavaScript, React, Python, Flask, Figma designing,
+        <p className="mt-4 text-gray-300">
+          Passionate software engineer skilled in HTML5, CSS, JavaScript, React, Python, Flask, Figma designing,
           database management, API development, SQL, MySQL, Firebase, GitHub, and Docker. Proficient in full-stack
           development, I excel in architecting, developing, and deploying complex software systems that deliver seamless user experiences. With
           expertise in these technologies, I build efficient web applications and contribute to the development of dynamic and interactive websites.
         </p>
       </div>
       <div className="md:w-1/2 flex justify-center md:justify-end">
-        <img src="/src/assets/Screenshot from 2024-05-15 13-41-15.png" alt="Profile" className="rounded-full w-48 h-48 md:w-64 md:h-64 object-cover shadow-lg border-4 border-teal-400" />
+        <img src={profileImage} alt="Profile" className="rounded-full w-48 h-48 md:w-64 md:h-64 object-cover shadow-lg border-4 border-teal-400" />
       </div>
     </div>
   );
 }
 
 export default Home;
-
 
